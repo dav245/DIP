fbd5677e372115ec6260cc7fd3e738259bf719d8
David Hrůza <dav245@seznam.cz>
2022-11-22 23:24:27 +0100

    can update only concept messages

--- backend/app/Http/Requests/Message/UpdateMessageRequest.php
+++ backend/app/Http/Requests/Message/UpdateMessageRequest.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Requests\Message;
 
+use App\Models\Message;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateMessageRequest extends FormRequest
 {
     use CommonMessageRules;
+
+    public function authorize()
+    {
+        /** @var Message $message */
+        $message = $this->route('message');
+
+        return !$message->sent;
+    }
 
     public function rules()
     {
